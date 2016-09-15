37b7ca72bb3e92be3e1c0c3591f2ca9461bf0e4b
OnlyElena <OnlyElena@mail.ru>
2016-09-15 09:35:55 +0800

    Rejected commit: Default

--- src/main/java/ru/portalPFR/security/SecurityWebApplicationInitializer.java
+++ src/main/java/ru/portalPFR/security/SecurityWebApplicationInitializer.java
@@ -1,7 +1,8 @@
 package ru.portalPFR.security;
-
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 /**
  * Created by 048ChubakovaEL on 19.08.2016.
  */
-public class SecurityWebApplicationInitializer {
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
 }
